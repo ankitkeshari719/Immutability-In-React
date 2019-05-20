f7dc51ca22f7b21ce4dd6604a08e3eb8175ac659
Ankit Keshari <ankit.keshari@gslab.com>
2019-05-20 13:14:15 +0530

    Update single object in  an array of object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [
+      { fieldName: "title", valid: false },
+      { fieldName: "description", valid: true },
+      { fieldName: "cityId", valid: false },
+      { fieldName: "hostDescription", valid: false }
+    ]
+  };
+
+  handleClick = () => {
+    var newData = this.state.data.map(el => {
+      if (el.fieldName === "cityId")
+        return Object.assign({}, el, { valid: true });
+      return el;
+    });
+    this.setState({ data: newData });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me </button>
+      </div>
+    );
+  }
 }
-
 export default App;
